51d6f70cb5e6c4e682c42413062f935a9a693141
ThiasD <thethiasdavid@gmail.com>
2020-01-29 14:26:58 -0800

    added floor file and floors into main

---
+++ floor.js
@@ -0,0 +1,26 @@
+function Floor(game, spritesheet, xPosition, yPosition, /*length, height,*/ move){
+    this.x = xPosition;
+    this.y = yPosition;
+    //this.moving = move;
+    // this.length = length;
+    // this.height = height;
+    Entity.call(this, game, this.x, this.y);
+    this.spritesheet = spritesheet;
+    this.ctx = game.ctx;
+}
+
+Floor.prototype = new Entity();
+Floor.prototype.constructor = Floor;
+
+Floor.prototype.update = function () {
+    // if(this.moving === true) {
+    //     this.x += this.game.clockTick * this.speed;
+    //     if (this.x > 800) this.x = -230;
+    //     Entity.prototype.update.call();
+    // }
+}
+
+Floor.prototype.draw = function (ctx) {
+    ctx.drawImage(this.spritesheet, this.x, this.y);
+    Entity.prototype.draw.call(this);
+}
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("img/Background.png");
+ASSET_MANAGER.queueDownload("img/smallPlatform.png");
 
 ASSET_MANAGER.downloadAll(function () {
     var canvas = document.getElementById('gameWorld');
@@ -8,9 +9,29 @@ ASSET_MANAGER.downloadAll(function () {
 
     var gameEngine = new GameEngine();
     var bg = new Background(gameEngine, ASSET_MANAGER.getAsset("img/Background.png"));
+
+    //Floors are about 190 in length ; 187 creates a nice "repeat" look
+    var f1 = new Floor(gameEngine, ASSET_MANAGER.getAsset("./img/smallPlatform.png"), 0, 500, false);
+    var f2 = new Floor(gameEngine, ASSET_MANAGER.getAsset("./img/smallPlatform.png"), 187, 500, false);
+    var f3 = new Floor(gameEngine, ASSET_MANAGER.getAsset("./img/smallPlatform.png"), 374, 500, false);
+    var f4 = new Floor(gameEngine, ASSET_MANAGER.getAsset("./img/smallPlatform.png"), 561, 500, false);
+    var f5 = new Floor(gameEngine, ASSET_MANAGER.getAsset("./img/smallPlatform.png"), 748, 500, false);
+    var f6 = new Floor(gameEngine, ASSET_MANAGER.getAsset("./img/smallPlatform.png"), 935, 500, false);
+
+    var f7 = new Floor (gameEngine, ASSET_MANAGER.getAsset("./img/smallPlatform.png"), 465, 300, false);
+
     var kayNine = new KayNine(gameEngine);
 
     gameEngine.addEntity(bg);
+
+    gameEngine.addEntity(f1);
+    gameEngine.addEntity(f2);
+    gameEngine.addEntity(f3);
+    gameEngine.addEntity(f4);
+    gameEngine.addEntity(f5);
+    gameEngine.addEntity(f6);
+    gameEngine.addEntity(f7);
+
     gameEngine.addEntity(kayNine);
 
     gameEngine.init(ctx);
