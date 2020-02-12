b12af91f0aa8cd42b402e9228942c7d641343a17
Patrick <bluedestiny88@gmail.com>
2020-02-12 14:01:27 -0800

    adding the vaccuum class and images

---
+++ vacuum.js
@@ -0,0 +1,25 @@
+function Vaccuum(game, spritesheet, xPosition, yPosition, width, height){
+    Entity.call(this, game, xPosition, yPosition, width, height, this);
+    this.spritesheet = spritesheet;
+    this.ctx = game.ctx;
+
+    // collision behavior can mimic that of a vaccuum
+    // the vaccuum moves along a platform like an enemy
+    this.type = "vaccuum";
+}
+
+Lazer.prototype = new Entity();
+Lazer.prototype.constructor = Floor;
+
+Lazer.prototype.update = function () {
+
+    
+
+}
+
+
+Lazer.prototype.draw = function (ctx) {
+    ctx.drawImage(this.spritesheet,
+                    this.xPos, this.yPos);
+    Entity.prototype.draw.call(this);
+}
