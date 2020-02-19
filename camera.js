001d3c1e7620c562f0dfcd5bdb3eb031fc944620
Patrick <bluedestiny88@gmail.com>
2020-02-19 15:00:10 -0800

    Fixing camera.

--- camera.js
+++ camera.js
@@ -1,4 +1,4 @@
-function Camera (game) {
+function Camera (game, kaynine) {
 
 	//	constructor
 	this.type = "camera";
@@ -6,6 +6,8 @@ function Camera (game) {
 	this.camY = 0;
 	this.width = 800;
 	this.height = 800;
+	this.kaynine = kaynine;	//	adds KayNine to object
+	this.boundingbox = new boundingbox(this.x + 200, this.y + 200, 400, 400);	// creates camera bounding box that KayNine interacts with
 	game.updateCam(this);
 	
 	//	Makes Camera object an entity
@@ -20,21 +22,22 @@ function Camera (game) {
 	
 	Camera.prototype.update = function ()	{
 		
-		this.boundingbox = new boundingbox(this.x + 200, this.y + 200, 400, 400);
-		
-		// if kaynine does not collide with bounding box
-		if("kaynine".xPos < camera.boundingbox.left) this.x("kaynine".xPos,  camY);
-		if("kaynine".xPos > camera.boundingbox.right) this.x("kaynine".xPos + "kaynine".width,  camY);
-		if("kaynine".yPos < camera.boundingbox.top) this.x(camX, "kaynine".yPos + "kaynine".height);
-		if("kaynine".yPos > camera.boundingbox.bottom) this.x(camX, "kaynine".yPos);
+		this.boundingbox.update(this.xPos + 200, this.y + 100);
+
+		if(this.boundingbox.collide(this.kaynine.boundingbox)) {
+			if(.xPos < camera.boundingbox.left) this.x(kaynine.xPos,  camY);
+			if(kaynine.xPos > camera.boundingbox.right) this.x(kaynine.xPos + kaynine.width,  camY);
+			if(kaynine.yPos < camera.boundingbox.top) this.x(camX, kaynine.yPos + kaynine.height);
+			if(kaynine.yPos > camera.boundingbox.bottom) this.x(camX, kaynine.yPos);
+		}
 	
 	
 	}
 	
 	function updatePos (x, y)	{
-	"camera".camX = x;
-	"camera".camY = y;
-	"camera".boundingbox.update(x+2, y+2)
+	camera.camX = x;
+	camera.camY = y;
+	camera.boundingbox.update(x+2, y+2)
 	game.updateCam(this);
 	
 
